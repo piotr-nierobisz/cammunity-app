5383f3541836644e1c715d5cef69a244d439bc3a
Cammunity-freelance <freelance@cammunity.co.uk>
2023-03-07 23:34:30 +0100

    Added Navigation and routes for none logged users

--- components/navigations/SignedOutNavigation/MainStackNavigator.tsx
+++ components/navigations/SignedOutNavigation/MainStackNavigator.tsx
@@ -2,7 +2,20 @@ import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Globals, Screen } from "@app/constants";
-import { EquipmentsScreen, HomeScreen } from "@app/components/screens";
+import {
+	EquipmentsScreen,
+	HomeScreen,
+	SingleEquipmentScreen,
+	EquipmentBookingScreen,
+	HowItWorksScreen,
+	AboutScreen,
+	FaqsScreen,
+	ContactScreen,
+	PrivacyPolicyScreen,
+	TermsOfServiceScreen,
+	SignUpScreen,
+	SignInScreen,
+} from "@app/components/screens";
 
 const Stack = createNativeStackNavigator();
 
@@ -35,6 +48,86 @@ const MainStackNavigator = () => {
 		{
 			name: EQUIPMENTS,
 			component: EquipmentsScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: SINGLE_EQUIPMENT,
+			component: SingleEquipmentScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: EQUIPMENT_BOOKING,
+			component: EquipmentBookingScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: HOW_IT_WORKS,
+			component: HowItWorksScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: ABOUT_US,
+			component: AboutScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: FAQ,
+			component: FaqsScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: CONTACT,
+			component: ContactScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: PRIVACY_POLICY,
+			component: PrivacyPolicyScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: TERMS_OF_SERVICE,
+			component: TermsOfServiceScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: SIGN_UP,
+			component: SignUpScreen,
+			options: {
+				headerShown: false,
+				title: "",
+			},
+		},
+		{
+			name: SIGN_IN,
+			component: SignInScreen,
 			options: {
 				headerShown: false,
 				title: "",
--- components/screens/index.ts
+++ components/screens/index.ts
@@ -1,2 +1,14 @@
 export * from './HomeScreen/HomeScreen';
 export * from './EquipmentsScreen/EquipmentsScreen';
+export * from './SingleEquipmentScreen/SingleEquipmentScreen';
+export * from './EquipmentBookingScreen/EquipmentBookingScreen';
+export * from './AboutScreen/AboutScreen';
+export * from './HowItWorksScreen/HowItWorksScreen';
+
+export * from './FaqsScreen/FaqsScreen';
+export * from './ContactScreen/ContactScreen';
+export * from './PrivacyPolicyScreen/PrivacyPolicyScreen';
+export * from './TermsOfServiceScreen/TermsOfServiceScreen';
+export * from './CsrScreen/CsrScreen';
+export * from './SignUpScreen/SignUpScreen';
+export * from './SignInScreen/SignInScreen';
